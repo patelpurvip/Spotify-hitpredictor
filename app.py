662a5428a8e371e627b925f8216b264b567a421b
Cris B <cristina.bardan@gmail.com>
2020-07-01 21:42:53 -0500

    spotify with new models and scaler

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import spotify
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Route to render index.html
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+# Route for hit_flop
+@app.route('/hit_flop/')
+def hit_flop():
+    track = track
+    artist = artist
+    hit_predict, hit_score = spotify.hit_flop(track, artist)
+    return redirect("/", hit_predict=hit_predict, hit_predict=hit_predict)
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
