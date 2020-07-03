db64243ec9369caa873242f9c9b38608397e8cff
Cris B <cristina.bardan@gmail.com>
2020-07-02 21:45:47 -0500

    some file cleaning

--- spotify.py
+++ spotify.py
@@ -24,33 +24,74 @@ def hit_flop(track, artist):
     artist = artist.lower()
 
     # Call song
+
     spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
-    results = spotify.search(q='track:' + track, type='track')
-
-    # Find song uri by matching artist
-    items = results['tracks']['items']
-    for i in items:
-        item_artist = i['artists'][0]['name']
-        if item_artist.lower() == artist:
-            uri = i['uri']
-
-    # Setting information from audio analysis
-    audio_analysis = spotify.audio_analysis(track_id=uri)
-    count_sections = len(audio_analysis['sections'])
-    chorus_hit = audio_analysis['sections'][2]['start']
     
-    # Setting information from audio features
-    audio_features = spotify.audio_features(tracks=[uri])
-    f = audio_features[0]
+    try:
+        results = spotify.search(q='track:' + track, type='track')
+        
+        # Find song uri by matching artist
+        items = results['tracks']['items']
+        for i in items:
+            item_artist = i['artists'][0]['name']
+            if item_artist.lower() == artist:
+                uri = i['uri']
 
-    # Arrange data to load to model
-    x = [[f['danceability'],f['energy'],f['key'],f['loudness'],f['mode'],f['speechiness'],f['acousticness'],f['instrumentalness'],f['liveness'],f['valence'],f['tempo'],f['duration_ms'],f['time_signature'],chorus_hit,count_sections]]
+        # Setting information from audio analysis
+        audio_analysis = spotify.audio_analysis(track_id=uri)
+        count_sections = len(audio_analysis['sections'])
+        chorus_hit = audio_analysis['sections'][2]['start']
+        
+        # Setting information from audio features
+        audio_features = spotify.audio_features(tracks=[uri])
+        f = audio_features[0]
 
-    # Scaling data
-    x_scaled = scaler.transform(x)
+        # Arrange data to load to model
+        x = [[
+            f['danceability'],
+            f['energy'],
+            f['key'],
+            f['loudness'],
+            f['mode'],
+            f['speechiness'],
+            f['acousticness'],
+            f['instrumentalness'],
+            f['liveness'],
+            f['valence'],
+            f['tempo'],
+            f['duration_ms'],
+            f['time_signature'],
+            chorus_hit,
+            count_sections]]
+        
+        # Create dictionary of data
+        x_dic = {
+            'danceability': f['danceability'],
+            'energy': f['energy'],
+            'key': f['key'],
+            'loudness': f['loudness'],
+            'mode': f['mode'],
+            'speechiness': f['speechiness'],
+            'acousticness': f['acousticness'],
+            'instrumentalness': f['instrumentalness'],
+            'liveness': f['liveness'],
+            'valence': f['valence'],
+            'tempo': f['tempo'],
+            'duration_ms': f['duration_ms'],
+            'time_signature': f['time_signature'],
+            'chorus_hit': chorus_hit,
+            'count_sections': count_sections
+            }
 
-    # Running model
-    hit_predict = model.predict(x_scaled)
-    hit_score = model.predict_proba(x_scaled)
+        # Scaling data
+        x_scaled = scaler.transform(x)
 
-    return hit_predict, hit_score
+        # Running model
+        hit_predict = model.predict(x_scaled)
+        hit_score = model.predict_proba(x_scaled)
+
+        return [hit_predict, hit_score, x_dic]
+    
+    except:
+        error = "We can't find your entry in the spotify database, please check your spelling and/or try again"
+        return [error] 
