0a3b5ae1842ad1b0cc59873bfe1c440d3dc6b265
Cris B <cristina.bardan@gmail.com>
2020-07-04 13:41:19 -0500

    test heroku

--- app.py
+++ app.py
@@ -41,8 +41,12 @@ def hit_flop():
         else:
             error = response[0]
             return render_template("index.html", score=error, item='home')
-    except:
-        return response   
+    except Exception as inst:
+        print(type(inst))    # the exception instance
+        print(inst.args)     # arguments stored in .args
+        print(inst)          # __str__ allows args to be printed directly,
+                             # but may be overridden in exception subclasses
+                             # unpack args
 
 @app.route("/about/")
 def about():
